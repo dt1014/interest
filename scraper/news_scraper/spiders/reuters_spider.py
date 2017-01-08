fe9b7c710d7d8e6eed02799f69fb37a2fddcb5d5
da-tash <daigo_t_1014@yahoo.co.jp>
2017-01-08 23:23:41 +0900

    Update little

--- scraper/news_scraper/spiders/reuters_spider.py
+++ scraper/news_scraper/spiders/reuters_spider.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 from datetime import datetime
+import logging
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
@@ -11,27 +12,34 @@ class ReutersSpider(CrawlSpider):
     name = 'reuters'
     allowed_domains = ['jp.reuters.com']
     start_urls = ['http://jp.reuters.com/investing/currencies',
+                  'http://jp.reuters.com/news',
+                  'http://jp.reuters.com/topNews',
                   'http://jp.reuters.com/news/global-economy',
                   'http://jp.reuters.com/news/world',
                   'http://jp.reuters.com/news/business',
-                  'http://jp.reuters.com/news/technology']
+                  'http://jp.reuters.com/news/technology',
+                  'http://jp.reuters.com/news/archive/']
+    start_urls += ['http://jp.reuters.com/search/news?sortBy=&dateRange=&blob=%d'%x for x in range(2000, 2016)]
     
-    deny_under_domain = ['video',
-                         'info',
-                         'tools',
-                         'blog']
-
     def process_for_multi_pages(value):
 
+        url_logger = logging.getLogger('url_process')
+        
         if 'http://internal.jp.reuters.com/' in value:
-            value = 'http://jp.reuteres.com/' + value.split('/', 3)[3]
-
+            temp = 'http://jp.reuteres.com/' + value.split('/', 3)[3]
+            url_logger.info('replace domain: <%s> to <%s>' % (value, temp))
+            value = temp
+            
         if 'http://jp.reuters.com/article/' in value:
-            return value.split('?pageNumber=')[0] + '?sp=true'
+            if '?sp=true' in value:
+                return value
+            else:
+                return value.split('?pageNumber=')[0] + '?sp=true'
         else:
             return value
     
-    rules = [Rule(LinkExtractor(deny=['http://jp.reuters.com/(%s).*$' % '|'.join(deny_under_domain), 'http://jp.reuters.com/article.*'])),
+    rules = [Rule(LinkExtractor(deny=['http://jp.reuters.com/(%s).*$' % '|'.join(['video', 'info', 'tools', 'article', 'investing'])])),
+             Rule(LinkExtractor(allow=['http://jp.reuters.com/(%s)/$' % '|'.join(['investing', 'investing/news'])])),
              Rule(LinkExtractor(allow=['http://jp.reuters.com/article.*'],
                                 deny=['^.*\?pageNumber=([2-9]|[1-9][0-9]).*$', '^.*\?sp=true.+$'],
                                 process_value=process_for_multi_pages),
@@ -40,7 +48,6 @@ class ReutersSpider(CrawlSpider):
 
     
     def parse_articles(self, response):
-        print('****************************************')
         url = response.url
         item = NewsItem()
         item['URL'] = url
@@ -48,12 +55,24 @@ class ReutersSpider(CrawlSpider):
         if m:
             item['ID'] = 'JP' + m.group(1)
         else:
-            item['ID'] = None
-            self.logger.error('Cannot parse ID from url(%s)', url)
+            m = re.search('idJP(.*)$', url)
+            if m:
+                item['ID'] = 'JP' + m.group(1)
+            else:
+                item['ID'] = None
+                self.logger.error('Cannot parse ID from url(%s)', url)
         item['category'] = response.xpath('//*[@class="article-section"]/text()').extract()[0]
         item['title'] = response.xpath('//h1[@class="article-headline"]/text()').extract()[0].replace('\u3000', ' ')
         item['content'] = ''.join([x.replace('\u3000', ' ') for x in response.xpath('//*[@id="articleText"]//p/text()').extract()])
         item['publication_datetime'] = response.xpath('//*[@class="article-header"]//*[@class="timestamp"]/text()').extract()[0]
         item['scraping_datetime'] = datetime.now().strftime('%Y年 %m月 %d日 %H:%m JST')
 
+        self.logger.info('scraped from <%s> published in %s' % (item['URL'], item['publication_datetime']))
+        
+        print('****************************************')
+        print('url     : ', item['URL'])
+        print('id      : ', item['ID'])
+        print('category: ', item['category'])
+        print('title   : ', item['title'])
+        
         return item
