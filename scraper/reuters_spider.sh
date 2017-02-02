d03a0796aae7aaa4dcb9947604da015f7294be22
田代大悟 <daigo@daigonombp.local>
2017-02-02 19:59:40 +0900

    Change how to log

--- scraper/reuters_spider.sh
+++ scraper/reuters_spider.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-mkdir -p output
+mkdir -p log
+
+run_datetime=`date '+%y%m%d-%H%M'`
+logfile_name="reuters_${run_datetime}.log"
+
 scrapy crawl reuters \
-	   --logfile=output/reuters_spider.log \
+	   --logfile=log/${logfile_name} \
 	   --loglevel=INFO
