fadc97a5d84c70adfd2f58719116cddb26469f68
田代大悟 <daigo@daigonombp.local>
2017-02-02 19:58:00 +0900

    Cosmetic change of regex

--- scraper/news_scraper/spiders/reuters_spider.py
+++ scraper/news_scraper/spiders/reuters_spider.py
@@ -51,28 +51,20 @@ class ReutersSpider(CrawlSpider):
         url = response.url
         item = NewsItem()
         item['URL'] = url
-        m = re.search('idJP(.*?)\?', url)
+        m = re.search('(idJP[^\?]*)', url)
         if m:
-            item['ID'] = 'JP' + m.group(1)
+            item['ID'] = 'JP' + m.group(0)
         else:
-            m = re.search('idJP(.*)$', url)
-            if m:
-                item['ID'] = 'JP' + m.group(1)
-            else:
-                item['ID'] = None
-                self.logger.error('Cannot parse ID from url(%s)', url)
+            item['ID'] = None
+            self.logger.error('Cannot parse ID from url(%s)', url)
+
         item['category'] = response.xpath('//*[@class="article-section"]/text()').extract()[0]
         item['title'] = response.xpath('//h1[@class="article-headline"]/text()').extract()[0].replace('\u3000', ' ')
         item['content'] = ''.join([x.replace('\u3000', ' ') for x in response.xpath('//*[@id="articleText"]//p/text()').extract()])
         item['publication_datetime'] = response.xpath('//*[@class="article-header"]//*[@class="timestamp"]/text()').extract()[0]
-        item['scraping_datetime'] = datetime.now().strftime('%Y年 %m月 %d日 %H:%m JST')
+        item['scraping_datetime'] = datetime.now().strftime('%Y年 %m月 %d日 %H:%M JST')
 
         self.logger.info('scraped from <%s> published in %s' % (item['URL'], item['publication_datetime']))
-        
-        print('****************************************')
-        print('url     : ', item['URL'])
-        print('id      : ', item['ID'])
-        print('category: ', item['category'])
-        print('title   : ', item['title'])
+
         
         return item
