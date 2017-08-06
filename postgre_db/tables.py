be20dfbec48611376d26f1dff2c981c1f8a6761e
DaigoTashiro <daigo.tashiro@gmail.com>
2017-08-06 09:59:23 +0900

    Change database type

--- postgre_db/tables.py
+++ postgre_db/tables.py
@@ -8,8 +8,8 @@ class ReutersArticle(Base):
 
     ID = Column('id', Integer, primary_key=True)
     URL = Column('url', Text())
-    category = Column('category', String(63))
-    title = Column('title', String(63))
+    category = Column('category', String(127))
+    title = Column('title', String(127))
     content = Column('content', Text())
     publication_datetime = Column('publication_datetime', DateTime())
     scraping_datetime = Column('scraping_datetime', DateTime())
@@ -17,9 +17,9 @@ class ReutersArticle(Base):
 class BBCArticle(Base):
     __tablename__ = 'bbc'
 
-    ID = Column('id', Integer, primary_key=True)
+    ID = Column('id', String(63), primary_key=True)
     URL = Column('url', Text())
-    title = Column('title', String(63))
+    title = Column('title', String(127))
     introduction = Column('introduction', Text())
     content = Column('content', Text())
     publication_datetime = Column('publication_datetime', DateTime())
@@ -28,11 +28,11 @@ class BBCArticle(Base):
 class ITMediaArticle(Base):
     __tablename__ = 'itmedia'
 
-    ID = Column('id', Integer, primary_key=True)
+    ID = Column('id', String(63), primary_key=True)
     URL = Column('url', Text())
     category = Column('category', String(63))
     page_count = Column('page_count', String(3))
-    title = Column('title', String(63))
+    title = Column('title', String(127))
     introduction = Column('introduction', Text())
     content = Column('content', Text())
     publication_datetime = Column('publication_datetime', DateTime())
