0de81ce3148ab58d6e2ee8347e019f37685355e0
DaigoTashiro <daigo.tashiro@gmail.com>
2017-09-08 11:05:11 +0900

    Update processing logic

--- preprocess.sh
+++ preprocess.sh
@@ -2,9 +2,48 @@
 
 working_dir=makeDataSet/preprocessing
 result_dir=${working_dir}/result
+log_dir=${working_dir}/log
+target_list=${working_dir}/target_list.txt
 
-mkdir -p ${result_dir}
+mkdir -p ${log_dir}
 
-python ${working_dir}/process.py \
-    --outpath ${result_dir} \
-    --logpath ${result_dir}
+for option in "$@"
+do
+    case "${option}" in
+	"-t"|"--target" )
+	    target="${2}"
+	    shift 2
+    esac
+done
+
+function processCommonFromDB() {
+    
+    mkdir -p ${result_dir}/${target}
+   
+    logpath=${log_dir}/${target}.log
+    if [ -f ${logpath} ]
+    then
+	rm ${logpath}
+    fi
+
+    python ${working_dir}/processCommonFromDB.py \
+	--outpath ${result_dir}/${target}/common_procesed.pkl \
+	--logpath ${logpath} \
+	--target ${target}
+
+}
+
+function pipe() {
+    processCommonFromDB ${target}  
+}
+
+if [ -z "${target}" ]
+then
+    for target in `cat ${target_list}`
+    do
+	pipe ${target}
+    done
+else
+    pipe ${target}
+fi
+
