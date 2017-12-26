c0f907f60ff683d78f266112995c63e9e9357ded
DaigoTashiro <daigo.tashiro@gmail.com>
2017-12-26 15:55:21 +0900

    Add length filter for target

--- chainer/ABS/dataset/src/dataset.py
+++ chainer/ABS/dataset/src/dataset.py
@@ -56,6 +56,9 @@ def main(args):
     df_id = pickle.load(open(os.path.join(args.inpath, "df_id.pkl"), "rb"))
     dictionary = pickle.load(open(os.path.join(args.inpath, "dictionary.pkl"), "rb"))
 
+    df_id = df_id.assign(length_x=lambda df: df["target"].apply(lambda x: len(x)))
+    df_id = df_id.iloc[np.where((df_id["length_x"]>args.config["min_length_x"]) & (df_id["length_x"]<args.config["max_length_x"]))[0]]
+    
     df_id_train, df_id_val, df_id_test = np.split(df_id.sample(frac=1, random_state=rng),
                                                   [int((1-args.config["val_test_ratio"])*len(df_id)), int((1-args.config["val_test_ratio"]/2.)*len(df_id))])
     
@@ -65,7 +68,7 @@ def main(args):
     val = pd.DataFrame(val, columns=["x", "yc", "t"])
     test = schedule_split(df_id_test, args.config, dictionary)
     test = pd.DataFrame(test, columns=["x", "yc", "t"])
-
+    
     save(train, val, test, df_id_train, df_id_val, df_id_test, dictionary, args.outdir)
     
     
