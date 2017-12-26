fed06f1cec3d5a64ce922c7cdd5ffbc57ce3b14a
DaigoTashiro <daigo.tashiro@gmail.com>
2017-12-26 13:13:36 +0900

    No more used

--- chainer/ABS/src/nnModel.py
+++ chainer/ABS/src/nnModel.py
@@ -45,72 +45,31 @@ class ABS(Chain):
         ### encoder ###
         xs = [Variable(self.xp.array(x, dtype=self.xp.int32)) for x, _ in x_yc]
         yc = Variable(self.xp.array([yc for _, yc in x_yc], dtype=self.xp.int32))
-
-        print("yc")
-        print(yc.shape)
         
         tilde_xs = [self.F(x) for x in xs]
-        print("tilde_xs")
-        print([i.shape for i in tilde_xs])
         
         tilde_dash_yc = self.G(yc)
-        print("tilde_dash_yc")
-        print(tilde_dash_yc.shape)
-        print(F.reshape(tilde_dash_yc, (50, -1)).shape)
         
         P_tilde_dash_yc = self.P(F.reshape(tilde_dash_yc, (tilde_dash_yc.shape[0], -1)))
-        print("P_tilde_dash_yc")
-        print(P_tilde_dash_yc.shape)
-
-        xPyc = [F.flatten(F.softmax(F.matmul(pyc, F.transpose(x)), axis=1)) for pyc, x in zip(F.split_axis(P_tilde_dash_yc, P_tilde_dash_yc.shape[0], axis=0), tilde_xs)]
-        print("xPyc")
-        print([i.shape for i in xPyc])
         
-        print("check")
-        print([F.expand_dims(F.expand_dims(x, axis=0), axis=3).shape for x in tilde_xs])
-
-        print("pad_size")
-        print(self.pad_size_former)
-        print(self.pad_size_latter)
+        xPyc = [F.flatten(F.softmax(F.matmul(pyc, F.transpose(x)), axis=1)) for pyc, x in zip(F.split_axis(P_tilde_dash_yc, P_tilde_dash_yc.shape[0], axis=0), tilde_xs)]
         
         padded_xs = [F.concat((F.concat((F.broadcast_to(x[0], (self.pad_size_former, self.H_hidden_size)), x), axis=0), F.broadcast_to(x[-1], (self.pad_size_latter, self.H_hidden_size))), axis=0) for x in tilde_xs]
 
-        print("tilde_xs")
-        print([i.shape for i in tilde_xs])
-        print("padded_xs")
-        print([i.shape for i in padded_xs])
-        
         bar_xs = [F.average_pooling_2d(F.expand_dims(F.expand_dims(x, axis=0), axis=1), ksize=(self.Q_smoothing_window, 1), stride=(1, 1), pad=0) for x in padded_xs]
-        print("bar_xs")
-        print([i.shape for i in bar_xs])
         bar_xs = [F.squeeze(x, axis=(0, 1)) for x in bar_xs]
-        print("tilde_xs")
-        print([i.shape for i in tilde_xs])
-        print("bar_xs")
-        print([i.shape for i in bar_xs])
         
         enc = F.concat([F.matmul(F.expand_dims(p, axis=0), bar_x) for p, bar_x in zip(xPyc, bar_xs)], axis=0)
-        print("enc")
-        print(enc.shape)
-        print([i.shape for i in enc])
+
+        if self.return_enc:
+            return enc
         
         ### NLM ###
         tilde_yc = self.E(yc)
-        print("tilde_yc")
-        print(tilde_yc.shape)
 
         h = self.activate(self.U(tilde_yc))
-        print("h")
-        print(h.shape)
-
 
         ### Integrate ###
         y = self.V(h) + self.W(enc)
-        print("y")
-        print(y.shape)
-        print(y)
-        
-        if self.return_enc:
-            pass
         
         return y
